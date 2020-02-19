206c9715a6a833e64790c4be8e41f1d479ba268b
Joseph Wilk <joe@josephwilk.net>
2020-02-19 19:52:06 +0000

    validate event

--- src/geo_event_logger/events.clj
+++ src/geo_event_logger/events.clj
@@ -3,27 +3,34 @@
             [geo-event-logger.db :as db]
 
             [clj-postgresql.spatial :as spatial]
-
             ))
 
 (defn all []
   (into [] (sql/query db/spec ["select * from events order by id desc"])))
 
+(defn valid? [event]
+  (and (contains? event :lng)
+       (contains? event :lat)))
 
 ;; * srid 4326
 ;;INSERT INTO events (geo) VALUES (ST_GeomFromText('POINT(-126.4 45.32)', 4326));
 
+
 (defn create [event]
-  (let [clean-event (->
-                     event
-                     (select-keys [:lng :lat :resource])
-                     (assoc :geo (spatial/point (:lng event) (:lat event)))
-                     (dissoc :lng)
-                     (dissoc :lat)
-                     )]
-    (sql/insert! db/spec :events clean-event)))
+  (when (valid? event)
+    (let [clean-event (->
+                       event
+                       (select-keys [:lng :lat :resource])
+                       (assoc :geo (spatial/point (:lng event) (:lat event)))
+                       (dissoc :lng)
+                       (dissoc :lat)
+                       )]
+      (sql/insert! db/spec :events clean-event))))
 
 (comment
+  (valid? {})
+  (valid? {:lng 1 :lat 2})
+
   (create {:lng 1 :lat 2})
   (all)
   )
