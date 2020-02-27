6430b870b4b2224560ac58ab223c10b4cd60ac94
Joseph Wilk <joe@josephwilk.net>
2020-02-27 15:31:18 +0000

    trust paredit

--- src/geo_event_logger/store.clj
+++ src/geo_event_logger/store.clj
@@ -3,4 +3,4 @@
 (defn bucket     [] (System/getenv "S3_BUCKET"))
 (defn aws-zone   [] (System/getenv "S3_ZONE"))
 (defn access-key [] (System/getenv "S3_ACCESS_KEY"))
-(defn secret-key [] (System/getenv "S3_SECRET_KEY")))
+(defn secret-key [] (System/getenv "S3_SECRET_KEY"))
